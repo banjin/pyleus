73e8ed7049a919efe11ff926ab5a764ac1871463
banjin <baliang616@163.com>
2018-01-25 11:22:44 +0800

    增加kafka数据计数功能

--- examples/kafka_spout_example/kafka_spout_example/log_results.py
+++ examples/kafka_spout_example/kafka_spout_example/log_results.py
@@ -2,17 +2,22 @@
 from pyleus.storm import SimpleBolt
 import os,re,time
 import logging
+from collections import namedtuple,defaultdict
 from hdfs.client import Client
 log = logging.getLogger('log_results')
 client = Client("http://localhost:50070")
-
+#Counter = namedtuple("Counter", "src_ip count")
 class LogResultsBolt(SimpleBolt):
+   
+    def initialize(self):
+        self.ips = defaultdict(int)
+
     def process_tuple(self, tup):
         log.info("v:{0}".format(tup.values)) 
         if tup.values:
-            log.info("dd")
             s = "{0}".format(tup.values[0])
-            log.info(s)
+            self.ips[s] += 1
+            log.info("ip,count:{0},{1}".format(s,self.ips[s]))
             #self.emit((s,), anchors=[tup])
             #with open('/home/qsadmin/stom-kafka.txt','a') as f:
             #    for i in data_list:
