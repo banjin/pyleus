bea05e48ae133feb189349a976dda29155c7da14
banjin <baliang616@163.com>
2018-01-30 09:51:06 +0800

    添加hdfs操作

--- examples/kafka_spout_example/kafka_spout_example/log_results.py
+++ examples/kafka_spout_example/kafka_spout_example/log_results.py
@@ -5,8 +5,7 @@ import logging
 from collections import namedtuple,defaultdict
 from hdfs.client import Client
 log = logging.getLogger('log_results')
-client = Client("http://localhost:50070")
-#Counter = namedtuple("Counter", "src_ip count")
+       
 class LogResultsBolt(SimpleBolt):
    
     def initialize(self):
@@ -18,6 +17,12 @@ class LogResultsBolt(SimpleBolt):
             s = "{0}".format(tup.values[0])
             self.ips[s] += 1
             log.info("ip,count:{0},{1}".format(s,self.ips[s]))
+            if not client.status("/hadoop/ttt.log",strict=False):
+                with client.write("/hadoop/ttt.log") as f:
+                    f.write("ip,count:{0},{1}".format(s,self.ips[s]))
+            else:
+                with client.write("/hadoop/ttt.log",append=True) as f:
+                    f.write("ip,count:{0},{1}".format(s,self.ips[s]))
             #self.emit((s,), anchors=[tup])
             #with open('/home/qsadmin/stom-kafka.txt','a') as f:
             #    for i in data_list:
@@ -30,4 +35,7 @@ if __name__=="__main__":
         filename='/tmp/file_results.log',
         format="%(message)s",
         filemode='a',)
+    client = Client("http://localhost:50070")
+    #Counter = namedtuple("Counter", "src_ip count")
+    #hdfs_dir_list = client.list("/hadoop")
     LogResultsBolt().run()
