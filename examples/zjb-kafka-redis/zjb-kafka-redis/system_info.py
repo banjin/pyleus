f9aebbaacdaca411c16617559d8786512ca2aa20
songhm <songhm@qssec.com>
2018-05-07 18:12:25 +0800

    统计次数和个数

--- examples/zjb-kafka-redis/zjb-kafka-redis/system_info.py
+++ examples/zjb-kafka-redis/zjb-kafka-redis/system_info.py
@@ -1,41 +1,36 @@
 # coding:utf-8
 from pyleus.storm import SimpleBolt
-import os,re,time
+import os, re, time
 import logging
-from collections import namedtuple,defaultdict
+from collections import namedtuple, defaultdict
 from hdfs.client import Client
 import redis
 import datetime
 from utils import IPS as ips
 from utils import SYS_IPS as sys_ips
+
 r = redis.Redis(host='127.0.0.1', port=6379, password='qssec.com', db=0)
 log = logging.getLogger('log_results')
-
-from utils import SYSTEM_TOP10 as realTimeMonitoring
 import MySQLdb
-db = MySQLdb.connect("localhost", "root", "123456.", "gab_event")
+db = MySQLdb.connect("localhost", "root", "123456.", "zjb_event")
 cursor = db.cursor()
-cursor.execute("SELECT ips.ip, system_name.`name` FROM ips INNER JOIN ips_system_name ON "
-               "ips_system_name.ips_id = ips.id INNER JOIN system_name ON "
-               "ips_system_name.systemname_id = system_name.id;")
-
-
-system_ips = ["2.2.2.2", "2.2.2.3"]
-
-log.info("ips", ips)
-log.info("sys_ips", sys_ips)
 
 
 class LogResultsBolt(SimpleBolt):
 
+    OUTPUT_FIELDS = ["src_ip", "dst_ip", "system_info"]
+
     def initialize(self):
         # self.today = datetime.datetime.now().strftime("%Y-%m-%d")
         # self.yesterday = (datetime.date.today() + datetime.timedelta(days=1)).strftime("%Y-%m-%d")
-        self.results = cursor.fetchall()
-        self.system_ip_list = [info[0] for info in self.results]
+        cursor.execute("SELECT ips.ip, system_name.`name` FROM ips INNER JOIN ips_system_name ON "
+                       "ips_system_name.ips_id = ips.id INNER JOIN system_name ON "
+                       "ips_system_name.systemname_id = system_name.id;")
+        results = cursor.fetchall()
 
     def process_tuple(self, tup):
-        log.info("v:{0}".format(tup.values)) 
+
+        log.info("v:{0}".format(tup.values))
         if tup.values:
             s = "{0}".format(tup.values[0])
             ips[s] += 1
@@ -49,24 +44,19 @@ class LogResultsBolt(SimpleBolt):
             #         f.write("ip,count:{0},{1}".format(s,self.ips[s]))
             # 直接存储到redis中
             # 判断是否存在当天的数值
-            # 总攻击IP个数和攻击次数
-            r.set("attack_detection", {"attack_ip_num": len(ips), "attack_count_num": sum(ips.values())})
-            # 重要系统攻击监测
-            if tup.values[1] in self.system_ip_list:
-                sys_ips[str(tup.values[1])] += 1
-                realTimeMonitoring.update({str(tup.values[1]): sys_ips[str(tup.values[1])]})
-                # r.set("realTimeMonitoring", {"attack_count_num": len(sys_ips), "attack_ip_num": sum(sys_ips.values())})
-                r.set("realTimeMonitoring", realTimeMonitoring)
-            r.set("{0}".format(s), "{0}".format(ips[s]))
 
-            
+            self.emit(word, self.words[word])
+
+
+
+
 if __name__ == "__main__":
     logging.basicConfig(
         level=logging.INFO,
         filename='/tmp/file_results.log',
         format="%(message)s",
-        filemode='a',)
+        filemode='a', )
     # client = Client("http://40.125.161.143:50070")
     Counter = namedtuple("Counter", "src_ip count")
-    #hdfs_dir_list = client.list("/hadoop")
+    # hdfs_dir_list = client.list("/hadoop")
     LogResultsBolt().run()
--- examples/zjb-kafka-redis/zjb-kafka-redis/utils.py
+++ examples/zjb-kafka-redis/zjb-kafka-redis/utils.py
@@ -8,3 +8,4 @@ from collections import defaultdict
 
 IPS = defaultdict(int)
 SYS_IPS = defaultdict(int)
+SYSTEM_TOP10 = dict()
