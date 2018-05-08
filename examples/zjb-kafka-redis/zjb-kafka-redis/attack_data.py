6cb50c754bf51ea3e4729f1edc6061b6c04dfb3b
songhm <songhm@qssec.com>
2018-05-08 16:31:19 +0800

    添加新的数据源

--- examples/zjb-kafka-redis/zjb-kafka-redis/attack_data.py
+++ examples/zjb-kafka-redis/zjb-kafka-redis/attack_data.py
@@ -54,7 +54,7 @@ class LogResultsBolt(SimpleBolt):
             if not attack_ip_num_set:
                 attack_ip_num_set = []
             print ">>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-            print "attack_ip_num_set.....",attack_ip_num_set
+            log.info("attack_ip_num_set.....",attack_ip_num_set)
             attack_ip_set = list(set(attack_ip_num_set).add(src_ip))
 
             # attack_ips[dst_ip].add(src_ip)
