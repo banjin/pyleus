0b1da81dced6681f3ec4dcc7cc163f496bc006ce
songhm <songhm@qssec.com>
2018-05-09 16:15:45 +0800

    增加白名单

--- examples/zjb-kafka-redis/zjb-kafka-redis/attack_data.py
+++ examples/zjb-kafka-redis/zjb-kafka-redis/attack_data.py
@@ -7,7 +7,7 @@ import ast
 from collections import namedtuple, deque
 from utils import IPS as ips, RDS,ATTACK_TYPE as att_type,attack_ips
 from utils import SYS_IPS as sys_ips
-from utils import get_system_ips
+from utils import get_system_ips, get_write_list
 
 log = logging.getLogger('log_results')
 
@@ -20,6 +20,8 @@ attack_data = {}
 attack_ip_info = {}
 r_attack_ip = {}
 type_info = {}
+
+only_attack_ip,only_attacked_port,only_attacked_ip,lost_port,lost_attacked_ip,lost_attack_ip,total_info = get_write_list()
 
 
 class LogResultsBolt(SimpleBolt):
@@ -36,71 +38,85 @@ class LogResultsBolt(SimpleBolt):
             # src_ip = "{0}".format(tup.values[0])
             # dst_ip = "{0}".format(tup.values(1))
 
-            # 先不用计算白名单
-            # 统计被攻击IP的次数
-
-            dst_total_info = RDS.get("attack_system")
-            if not dst_total_info:
-                dst_total_info = {}
+            if src_ip in only_attack_ip:
+                pass
+            elif dst_ip in only_attacked_ip:
+                pass
+            elif dst_port in only_attacked_port:
+                pass
+            elif (src_ip, dst_ip) in lost_port:
+                pass
+            elif (src_ip, dst_port) in lost_attacked_ip:
+                pass
+            elif (dst_ip, dst_port) in lost_attack_ip:
+                pass
+            elif (src_ip, dst_ip,dst_port) in total_info:
+                pass
             else:
 
-                dst_total_info = ast.literal_eval(dst_total_info)
-            dst_ip_info = dst_total_info.get(str(dst_ip))
-            if not dst_ip_info:
-                dst_ip_info = {}
+                # 统计被攻击IP的次数
 
-            attack_count_num = dst_ip_info.get("attack_count_num")
-            log.info("attack_count_num, {}".format(attack_count_num))
-            if not attack_count_num:
-                attack_count_num = 0
-            attack_count_num += 1
-            attack_ip_num_set = dst_ip_info.get("attack_ip_num")
-            log.info("attack_ip_num_set, {}".format(attack_ip_num_set))
+                dst_total_info = RDS.get("attack_system")
+                if not dst_total_info:
+                    dst_total_info = {}
+                else:
+                    dst_total_info = ast.literal_eval(dst_total_info)
+                dst_ip_info = dst_total_info.get(str(dst_ip))
+                if not dst_ip_info:
+                    dst_ip_info = {}
 
-            if attack_ip_num_set:
-                set(attack_ip_num_set).add(src_ip)
-                attack_ip_set = list(attack_ip_num_set)
-            else:
-                b = set()
-                b.add(src_ip)
-                attack_ip_set = list(b)
+                attack_count_num = dst_ip_info.get("attack_count_num")
+                log.info("attack_count_num, {}".format(attack_count_num))
+                if not attack_count_num:
+                    attack_count_num = 0
+                attack_count_num += 1
+                attack_ip_num_set = dst_ip_info.get("attack_ip_num")
+                log.info("attack_ip_num_set, {}".format(attack_ip_num_set))
 
-            # attack_ips[dst_ip].add(src_ip)
-            # log.info(ips[dst_ip])
-            # 每个被攻击ip的攻击次数
-            dst_total_info.update({str(dst_ip): {"attack_count_num": attack_count_num, "attack_ip_num": attack_ip_set}})
-            # 所有攻击ip
-            # attack_ip_info.setdefault(dst_ip, []).append(src_ip)
+                if attack_ip_num_set:
+                    set(attack_ip_num_set).add(src_ip)
+                    attack_ip_set = list(attack_ip_num_set)
+                else:
+                    b = set()
+                    b.add(src_ip)
+                    attack_ip_set = list(b)
 
-            attack_type_dict = RDS.get("attack_type")
-            if not attack_type_dict:
-                attack_type_dict = {}
-            else:
-                attack_type_dict = ast.literal_eval(attack_type_dict)
-            attack_type_num = attack_type_dict.get(attack_type)
-            if not attack_type_num:
-                attack_type_num = 0
+                # attack_ips[dst_ip].add(src_ip)
+                # log.info(ips[dst_ip])
+                # 每个被攻击ip的攻击次数
+                dst_total_info.update({str(dst_ip): {"attack_count_num": attack_count_num, "attack_ip_num": attack_ip_set}})
+                # 所有攻击ip
+                # attack_ip_info.setdefault(dst_ip, []).append(src_ip)
 
-            attack_type_num += 1
-            attack_type_dict.update({attack_type: attack_type_num})
-            # 攻击类型
-            RDS.set("attack_type", attack_type_dict)
-            RDS.set("attack_system", dst_total_info)
+                attack_type_dict = RDS.get("attack_type")
+                if not attack_type_dict:
+                    attack_type_dict = {}
+                else:
+                    attack_type_dict = ast.literal_eval(attack_type_dict)
+                attack_type_num = attack_type_dict.get(attack_type)
+                if not attack_type_num:
+                    attack_type_num = 0
 
-            # 实时数据
-            realtime_data = RDS.get("realtime_data")
-            if not realtime_data:
-                realtime_data = deque()
-            else:
-                realtime_data = deque(ast.literal_eval(realtime_data))
-            if len(realtime_data) < 10:
-                realtime_data.append({"src_ip":src_ip,"dst_ip":dst_ip, "attack_type": attack_type, "time": post_time})
-            else:
-                realtime_data.popleft()
-                realtime_data.append({"src_ip":src_ip,"dst_ip":dst_ip, "attack_type": attack_type, "time": post_time})
-            realtime_data = list(realtime_data)
+                attack_type_num += 1
+                attack_type_dict.update({attack_type: attack_type_num})
+                # 攻击类型
+                RDS.set("attack_type", attack_type_dict)
+                RDS.set("attack_system", dst_total_info)
 
-            RDS.set("realtime_data", realtime_data)
+                # 实时数据
+                realtime_data = RDS.get("realtime_data")
+                if not realtime_data:
+                    realtime_data = deque()
+                else:
+                    realtime_data = deque(ast.literal_eval(realtime_data))
+                if len(realtime_data) < 10:
+                    realtime_data.append({"src_ip":src_ip,"dst_ip":dst_ip, "attack_type": attack_type, "time": post_time})
+                else:
+                    realtime_data.popleft()
+                    realtime_data.append({"src_ip":src_ip,"dst_ip":dst_ip, "attack_type": attack_type, "time": post_time})
+                realtime_data = list(realtime_data)
+
+                RDS.set("realtime_data", realtime_data)
 
 
             # 可以不统计总的，接口中计算
--- examples/zjb-kafka-redis/zjb-kafka-redis/utils.py
+++ examples/zjb-kafka-redis/zjb-kafka-redis/utils.py
@@ -20,14 +20,14 @@ WAF_IPS = defaultdict(int)
 # waf 攻击重要系统的ip个数和次数
 WAF_SYS_IPS = defaultdict(dict)
 
+db = MySQLdb.connect("127.0.0.1", "root", "qssec.com", "gab_event")
+cursor = db.cursor()
+
 
 def get_system_ips():
     """"
     获取重要系统的所有IP
     """
-
-    db = MySQLdb.connect("127.0.0.1", "root", "qssec.com", "gab_event")
-    cursor = db.cursor()
     cursor.execute("SELECT ips.ip, system_name.`name` FROM ips INNER JOIN ips_system_name ON "
                    "ips_system_name.ips_id = ips.id INNER JOIN system_name ON "
                    "ips_system_name.systemname_id = system_name.id;")
@@ -39,11 +39,40 @@ def get_system_ips():
 
 def get_write_list():
     """
-    获取白名单
+    获取白名单,只记录通过白名单过滤后的数据
     :return:
     """
+    cursor.execute("select attack_ip,attacked_ip,port from white_list")
 
-    pass
+    results = cursor.fetchall()
+    #  (('1.1.1.1', '2.2.2.2', 60L),)
+    only_attack_ip = []
+    only_attacked_port = []
+    only_attacked_ip = []
+    lost_port = []
+    lost_attacked_ip = []
+    lost_attack_ip = []
+    total_info = []
+
+    for info in results:
+        # 只有attack_ip
+        if info[0] and info[1] and info[2]:
+            total_info.append(info)
+        elif info[0] and info[1] and not info[2]:
+            lost_port.append((info[0],info[1]))
+        elif info[0] and not info[1] and info[2]:
+            lost_attacked_ip.append((info[0],info[2]))
+        elif not info[0] and info[1] and info[2]:
+            lost_attack_ip.append((info[1],info[2]))
+        elif info[0] and not info[1] and not info[2]:
+            only_attack_ip.append(info[0])
+        elif not info[0] and info[1] and not info[2]:
+            only_attacked_port.append(info[1])
+        elif not info[0] and not info[1] and info[2]:
+            only_attacked_ip.append(info[2])
+
+    return only_attack_ip,only_attacked_port,only_attacked_ip,lost_port,lost_attacked_ip,lost_attack_ip,total_info
+
 
 
 def get_region_list():
