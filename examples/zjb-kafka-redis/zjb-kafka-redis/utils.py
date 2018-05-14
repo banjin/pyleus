48713457443933f547cc67e81b618ea0262774b9
songhm <songhm@qssec.com>
2018-05-14 16:15:27 +0800

    修改查询数据库时间

--- examples/zjb-kafka-redis/zjb-kafka-redis/utils.py
+++ examples/zjb-kafka-redis/zjb-kafka-redis/utils.py
@@ -6,7 +6,7 @@
 from collections import defaultdict
 import redis
 import MySQLdb
-
+import time
 IPS = defaultdict(int)
 attack_ips = defaultdict(set)
 ATTACK_TYPE = defaultdict(int)
@@ -21,20 +21,23 @@ WAF_IPS = defaultdict(int)
 WAF_SYS_IPS = defaultdict(dict)
 
 db = MySQLdb.connect("127.0.0.1", "root", "qssec.com", "zjb_event")
-cursor = db.cursor()
 
 
 def get_system_ips():
     """"
     获取重要系统的所有IP
     """
-    cursor.execute("SELECT ips.ip, system_name.`name` FROM ips INNER JOIN ips_system_name ON "
-                   "ips_system_name.ips_id = ips.id INNER JOIN system_name ON "
-                   "ips_system_name.systemname_id = system_name.id;")
+    while 1:
+        cursor = db.cursor()
+        cursor.execute("SELECT ips.ip, system_name.`name` FROM ips INNER JOIN ips_system_name ON "
+                       "ips_system_name.ips_id = ips.id INNER JOIN system_name ON "
+                       "ips_system_name.systemname_id = system_name.id;")
 
-    results = cursor.fetchall()
+        results = cursor.fetchall()
+        cursor.close()
+        time.sleep(10)
 
-    return [info[0] for info in results]
+        return [info[0] for info in results]
 
 
 def get_write_list():
@@ -42,36 +45,39 @@ def get_write_list():
     获取白名单,只记录通过白名单过滤后的数据
     :return:
     """
-    cursor.execute("select attack_ip,attacked_ip,port from white_list")
+    while 1:
+        cursor = db.cursor()
+        cursor.execute("select attack_ip,attacked_ip,port from white_list")
 
-    results = cursor.fetchall()
-    #  (('1.1.1.1', '2.2.2.2', 60L),)
-    only_attack_ip = []
-    only_attacked_port = []
-    only_attacked_ip = []
-    lost_port = []
-    lost_attacked_ip = []
-    lost_attack_ip = []
-    total_info = []
+        results = cursor.fetchall()
+        #  (('1.1.1.1', '2.2.2.2', 60L),)
+        only_attack_ip = []
+        only_attacked_port = []
+        only_attacked_ip = []
+        lost_port = []
+        lost_attacked_ip = []
+        lost_attack_ip = []
+        total_info = []
 
-    for info in results:
-        # 只有attack_ip
-        if info[0] and info[1] and info[2]:
-            total_info.append(info)
-        elif info[0] and info[1] and not info[2]:
-            lost_port.append((info[0],info[1]))
-        elif info[0] and not info[1] and info[2]:
-            lost_attacked_ip.append((info[0],info[2]))
-        elif not info[0] and info[1] and info[2]:
-            lost_attack_ip.append((info[1],info[2]))
-        elif info[0] and not info[1] and not info[2]:
-            only_attack_ip.append(info[0])
-        elif not info[0] and info[1] and not info[2]:
-            only_attacked_port.append(info[1])
-        elif not info[0] and not info[1] and info[2]:
-            only_attacked_ip.append(info[2])
-
-    return only_attack_ip,only_attacked_port,only_attacked_ip,lost_port,lost_attacked_ip,lost_attack_ip,total_info
+        for info in results:
+            # 只有attack_ip
+            if info[0] and info[1] and info[2]:
+                total_info.append(info)
+            elif info[0] and info[1] and not info[2]:
+                lost_port.append((info[0],info[1]))
+            elif info[0] and not info[1] and info[2]:
+                lost_attacked_ip.append((info[0],info[2]))
+            elif not info[0] and info[1] and info[2]:
+                lost_attack_ip.append((info[1],info[2]))
+            elif info[0] and not info[1] and not info[2]:
+                only_attack_ip.append(info[0])
+            elif not info[0] and info[1] and not info[2]:
+                only_attacked_port.append(info[1])
+            elif not info[0] and not info[1] and info[2]:
+                only_attacked_ip.append(info[2])
+        cursor.close()
+        time.sleep(60)
+        return only_attack_ip,only_attacked_port,only_attacked_ip,lost_port,lost_attacked_ip,lost_attack_ip,total_info
 
 
 
