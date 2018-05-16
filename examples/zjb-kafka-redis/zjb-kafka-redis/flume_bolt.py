21dcdf5c70618f683f3778149e65efaf4c130919
songhm <songhm@qssec.com>
2018-05-16 16:07:28 +0800

    修改获取地理位置

--- examples/zjb-kafka-redis/zjb-kafka-redis/flume_bolt.py
+++ examples/zjb-kafka-redis/zjb-kafka-redis/flume_bolt.py
@@ -21,6 +21,16 @@ class LogResultsBolt(SimpleBolt):
     def initialize(self):
         pass
 
+    address_map = {"00": "香港", "01": "安徽", "02": "浙江", "03": "江西", "04": "江苏",
+                   "05": "吉林", "06": "青海", "07": "福建", "08": "黑龙江",
+                   "09": "河南", "10": "河北", "11": "湖南", "12": "湖北",
+                   "13": "新疆", "14": "西藏", "15": "甘肃", "16": "广西",
+                   "18": "贵州", "19": "辽宁", "20": "内蒙古", "21": "宁夏",
+                   "22": "北京", "23": "上海", "24": "山西", "25": "山东",
+                   "26": "陕西", "28": "天津", "29": "云南", "30": "广东",
+                   "31": "海南", "32": "四川", "33": "重庆", "34": "澳门", "35": "台湾"
+                   }
+
     def process_tuple(self, tup):
         log.info("v:{0}".format(tup.values))
         if tup.values:
@@ -28,25 +38,31 @@ class LogResultsBolt(SimpleBolt):
             log.info("ip,count:{0}".format(s))
             rec = gi.record_by_addr(s)
             if not rec:
-                region = 0
+                region = ''
                 country_name = "未知"
                 city = "未知"
             else:
                 country_name = rec['country_name']
                 city = rec['city']
-                if not city:
-                    city = ''
+                region = rec['region_code']
+                if not region:
+                    region = ''
                 if country_name == "Macau":
                     region = 34
+                    city = "澳门"
                 elif country_name == "Taiwan":
                     region = 35
-                elif country_name == "China" or country_name == "Hong Kong":
+                    city = "台湾"
+                elif country_name == "China":
                     region = rec['region_code']
-                else:
-                    region = 37
+                    city = self.address_map.get(str(region), "其他")
+                elif country_name == "Hong Kong":
+                    region = "00"
+                    city = "香港"
                 if not region:
-                    region = 37
-            sock.sendall('{"ip": {src_ip}, "region": {region}, "city":{city}, "country_name":{country_name}}\n'.format(
+                    region = ''
+
+            sock.sendall('{"ip": {src_ip}, "region_code": {region}, "src_province":{city}, "country_name":{country_name}}\n'.format(
                 src_ip=s, region=str(region), city=city, country_name=country_name
             ))
 
